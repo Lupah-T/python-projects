6a51cede2f8ab93c6e56f461a66c87ab16223e8d
Lupah-T <isaackash254@gmail.com>
2025-06-25 06:01:13 -0900

    number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,8 +1,8 @@
 import random
-number = random.randint(1,20)
+number = random.randint(1,20) #takes a random integer between 1 and 20
 print("Im thinking of a number between 1 and 20....")
-while True:
-    guess = int(input("Enter you guessed number: "))
+while True: #while the conditions are true,then the code continues
+    guess = int(input("Enter you guessed number: ")) #prompts user to enter his/her guesed number
     if guess <number:
         print('Your guess is too low')
     elif guess > number:
